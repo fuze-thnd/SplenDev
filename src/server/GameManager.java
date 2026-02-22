22d2c33c18eb2d7587a36015244d6a08c5a33f6f
Thanadon Krajangroch <thanadon.thnd@gmail.com>
2026-02-22 08:23:36 +0700

    main structure

--- src/server/GameManager.java
+++ src/server/GameManager.java
@@ -12,12 +12,22 @@ public class GameManager {
     private DevelopmentCards[] cardsOnBoard = new DevelopmentCards[4];
     private Map<gemsColor, Integer> BankGems;
     public ArrayList<DevelopmentCards> level1Cards;
+    public ArrayList<DevelopmentCards> level2Cards;
+    public ArrayList<DevelopmentCards> level3Cards;
     
     public GameManager() {
         CardLoader cardLoader = new CardLoader();
         level1Cards = cardLoader.getLevel1();
+        level2Cards = cardLoader.getLevel2();
+        level3Cards = cardLoader.getLevel3();
         for (int i=0; i<40; i++) {
-            System.out.println(level1Cards.get(i).getId() +", "+ level1Cards.get(i).getGemsColor() +", "+ level1Cards.get(i).getPrestigePoints());
+            System.out.println(level1Cards.get(i).getId() +", "+ level1Cards.get(i).getGemsColor() +", "+ level1Cards.get(i).getPrestigePoints() + ", " + level1Cards.get(i).getCost());
+        }
+        for (int i=0; i<30; i++) {
+            System.out.println(level2Cards.get(i).getId() +", "+ level2Cards.get(i).getGemsColor() +", "+ level2Cards.get(i).getPrestigePoints() + ", " + level2Cards.get(i).getCost());
+        }
+        for (int i=0; i<20; i++) {
+            System.out.println(level3Cards.get(i).getId() +", "+ level3Cards.get(i).getGemsColor() +", "+ level3Cards.get(i).getPrestigePoints() + ", " + level3Cards.get(i).getCost());
         }
     }
 }
--- src/shared/CardLoader.java
+++ src/shared/CardLoader.java
@@ -17,17 +17,8 @@ public class CardLoader {
             
             for (int i=1; i<=40; i++) {
                 line = br.readLine();
-                String[] data = line.split(",");
                 
-                int prestigePoints = Integer.parseInt(data[2]);
-                HashMap cost = new HashMap();
-                cost.put(gemsColor.Black, Integer.valueOf(data[3]));
-                cost.put(gemsColor.Blue, Integer.valueOf(data[4]));
-                cost.put(gemsColor.Green, Integer.valueOf(data[5]));
-                cost.put(gemsColor.Red, Integer.valueOf(data[6]));
-                cost.put(gemsColor.White, Integer.valueOf(data[7]));
-                gemsColor bonusGemsColor = gemsColor.valueOf(data[1]);
-                level1.add(new DevelopmentCards(i, prestigePoints, cost, bonusGemsColor));
+                level1.add(getDevelopmentCardsData(i, line));
             }
         } catch (FileNotFoundException fileLost) {
             System.out.println(fileLost);
@@ -35,5 +26,61 @@ public class CardLoader {
             ex.printStackTrace();
         }
         return level1;
+    }
+    
+    public ArrayList<DevelopmentCards> getLevel2() {
+        level2 = new ArrayList<DevelopmentCards>();
+        try (BufferedReader br = new BufferedReader(new FileReader("AllCards.csv"))) {
+            String line;
+            line = br.readLine(); // skip the Header line
+            // skip the level1 cards line
+            for (int i=1; i<=40; i++)
+                line = br.readLine();
+            for (int i=41; i<=70; i++) {
+                line = br.readLine();
+                
+                level2.add(getDevelopmentCardsData(i, line));
+            }
+        } catch (FileNotFoundException fileLost) {
+            System.out.println(fileLost);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return level2;
+    }
+    
+    public ArrayList<DevelopmentCards> getLevel3() {
+        level3 = new ArrayList<DevelopmentCards>();
+        try (BufferedReader br = new BufferedReader(new FileReader("AllCards.csv"))) {
+            String line;
+            line = br.readLine(); // skip the Header line
+            // skip the level1 and level2 cards line
+            for (int i=1; i<=70; i++)
+                line = br.readLine();
+            for (int i=71; i<=90; i++) {
+                line = br.readLine();
+                
+                level3.add(getDevelopmentCardsData(i, line));
+            }
+        } catch (FileNotFoundException fileLost) {
+            System.out.println(fileLost);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return level3;
+    }
+    
+    public DevelopmentCards getDevelopmentCardsData(int id, String line) {
+        String[] data = line.split(",");
+        
+        int prestigePoints = Integer.parseInt(data[2]);
+        HashMap<gemsColor, Integer> cost = new HashMap<>();
+        cost.put(gemsColor.Black, Integer.valueOf(data[3]));
+        cost.put(gemsColor.Blue, Integer.valueOf(data[4]));
+        cost.put(gemsColor.Green, Integer.valueOf(data[5]));
+        cost.put(gemsColor.Red, Integer.valueOf(data[6]));
+        cost.put(gemsColor.White, Integer.valueOf(data[7]));
+        gemsColor bonusGemsColor = gemsColor.valueOf(data[1]);
+        return new DevelopmentCards(id, prestigePoints, cost, bonusGemsColor);
     }
 }
